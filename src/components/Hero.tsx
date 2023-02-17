884456ba76e53ca408fe5df4f390cd4e87053e30
Samuel Ventura <samuelventura.contato@gmail.com>
2023-02-17 08:46:22 -0300

    blur effect on background hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,8 +9,10 @@ export function Hero() {
           <source src="/background-hero.mp4" type="video/mp4"/>
         </video>
         <div className={styles.content}>
+          <span></span>
           <h1><strong>REVOLUCIONE</strong> A FORMA<br />COMO VOCÊ PRODUZ ENERGIA</h1>
           <p>Desenvolver soluções inovadoras e sustentáveis nunca foi tão fácil<br />com o auxílio da nossa inteligência artificial</p>
+          <span></span>
         </div>
         <div className={styles.button}>
           <button type="button" name="Botão para próxima sessão"><ArrowDown size="3.2rem" color="#D9D9D9" weight="regular" /></button>
